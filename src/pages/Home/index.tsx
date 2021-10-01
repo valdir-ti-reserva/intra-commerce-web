2d35ce1ce5f297175a8de139b4a3a6a14b96dd67
Valdir Silva <v.souza.da.silva@avanade.com>
2021-10-01 17:07:56 -0300

    feat(web): config files and first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Intracommerce App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
